6e573a4fbcb077eca7f5f7f802a8a4036eb2e5e9
daiki0381 <daiki03810218@gmail.com>
2022-03-22 00:19:43 +0900

    DBを利用したメモアプリの追加

---
+++ app.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# !/usr/bin/env ruby
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+connection = PG::Connection.new(dbname: 'sinatra')
+connection.exec('CREATE TABLE IF NOT EXISTS memo (
+  id serial primary key,
+  title varchar(20),
+  content varchar(200))')
+
+helpers do
+  include ERB::Util
+
+  def sanitize(text)
+    escape_html(text)
+  end
+end
+
+get '/memos' do
+  @memos = connection.exec('SELECT * FROM memo ORDER BY id DESC')
+  erb :top
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/memos' do
+  title = params[:title]
+  content = params[:content]
+  connection.exec('INSERT INTO memo (title, content) VALUES ($1, $2)', [title, content])
+  redirect to('/memos')
+end
+
+get '/memos/:id' do |id|
+  @memos = connection.exec('SELECT * FROM memo WHERE id = $1', [id])
+  erb :details
+end
+
+delete '/memos/:id' do |id|
+  connection.exec('DELETE FROM memo WHERE id = $1', [id])
+  redirect to('/memos')
+end
+
+get '/memos/:id/edit' do |id|
+  @memos = connection.exec('SELECT * FROM memo WHERE id = $1', [id])
+  erb :edit
+end
+
+patch '/memos/:id' do |id|
+  title = params[:title]
+  content = params[:content]
+  connection.exec('UPDATE memo SET title = $1, content = $2 WHERE id = $3', [title, content, id])
+  redirect to("memos/#{id}")
+end
